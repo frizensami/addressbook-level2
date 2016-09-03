145181b9e9e3e9a02bb7d0d3d155f4c347f28e5b
Sriram Sami <frizensami@gmail.com>
2016-09-04 00:31:33 +0800

    Add Printable class

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Defines getPrintableString that should return a 
+ * formatted string-representation of the implementing class.
+ * @author sriram
+ *
+ */
+public interface Printable {
+    public String getPrintableString();
+}
